46874ec5c8004301caa710ced7add09bfc2d8d9e
Matthew Rice <mtr.rice@gmail.com>
2016-06-07 15:09:37 -0400

    Completed Project.  Not happy with the responsiveness of my website.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,18 @@
 (function($){
   'use strict';
 
-  $(function() {
-    var $myHeading = $('.my-heading');
-    $myHeading.find(".sub-folders").not('.accordion').hide();
-    $myHeading.find(".sub-folders").eq(0).show();
+  $('.accordion > li h1').click(function(event){  //the .click function expression calls event which stands for event handler.
+    event.preventDefault();  //this may not be needed.  Not sure.
 
-    $myHeading.find(".accordion").click(function(){
-        $myHeading.find('.accordion').not(this).siblings().fadeOut(500);
-        $(this).siblings().fadeToggle(500);
-    }).eq(0).trigger('click');
-});
+    var $section = $(this).parent();  //not sure what "this" stands for.  This references the parent element of the .sub-folders
+    var $subFolders = $('.sub-folders');  //this variable declares a variable name for the html .sub-folders class.
+    console.log($('.sub-folders'));
+
+    $('.active').removeClass('active');  //this removes the .active class from the active .sub-folders
+    $section.find('.sub-folders').addClass('active'); //this finds the parent element of the active .sub-folders.
+
+    $subFolders.not('.active').slideUp(); //this finds the .sub-folders that aren't currently active and slides them up.
+    $('.active').slideDown();  //this will slide down the active .sub-folders.
+  });
 
 }(jQuery));
